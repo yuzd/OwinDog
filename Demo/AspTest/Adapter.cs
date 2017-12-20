e701cb01c121900dd7a7653f54af5b1e5dac0aff
zdyu <zdyu@Ctrip.com>
2017-12-20 14:13:51 +0800

    添加对aspx的支持测试

---
+++ Demo/AspTest/Adapter.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections.Generic;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using Owin.AspEngine;
+
+namespace AspTest
+{
+
+    /// <summary>
+    /// owin/owindog For OWIN 接口类
+    /// </summary>
+    public class Adapter
+    {
+
+
+        /// <summary>
+        /// OWIN适配器的主函数
+        /// </summary>
+        /// <param name="env"></param>
+        /// <returns></returns>
+        public Task OwinMain(IDictionary<string, object> env)
+        {
+
+            return AspNet.Process(env);
+        }
+
+
+
+
+
+        
+
+
+    }
+
+
+
+}
---
+++ Demo/AspTest/aspx/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.ipTxt.Value = GetIp();
+    }
+
+    public  string GetIp()
+    {
+        string result = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+        result += "," + HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+        result += "," + HttpContext.Current.Request.UserHostAddress;
+
+        if (string.IsNullOrEmpty(result) || !IsIPv4(result))
+        {
+            return "127.0.0.1";
+        }
+
+        return result;
+    }
+
+    public  bool IsIPv4(string ip)
+    {
+        return Regex.IsMatch(ip, @"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$");
+    }
+}
